b8ca7775d74933eb24a07216d4a22792e1faa8bb
Brendan Dahl <brendan.dahl@gmail.com>
2014-11-12 17:56:21 -0800

    Use unsigned for chars.

--- opt/jvm.ir.ts
+++ opt/jvm.ir.ts
@@ -173,6 +173,8 @@ module J2ME.C4.Backend {
         jsTypedArrayType = "Int32Array";
         break;
       case Kind.Char:
+        jsTypedArrayType = "Uint16Array";
+        break;
       case Kind.Short:
         jsTypedArrayType = "Int16Array";
         break;
